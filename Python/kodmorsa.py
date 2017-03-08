19d75e0d54a49cd2de87c5d6bebc5de70b86494f
Tomek2854 <twiniarski8@gmail.com>
2017-03-08 12:36:24 +0100

    Szyfr Cezara Python

---
+++ Python/kodmorsa.py
@@ -0,0 +1,14 @@
+kodmorsa.#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def koduj(napis):
+	litery = {'a': '*-','b': '-***'}
+	for litera
+
+def main(args):
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
