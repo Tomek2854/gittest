b1ef05bbc24524149bd61936a2a5d341659b870f
Tomek2854 <twiniarski8@gmail.com>
2018-01-11 13:28:32 +0100

    praca na lekcji

--- wskazniki.cpp
+++ wskazniki.cpp
@@ -15,8 +15,21 @@ int main(int argc, char **argv)
 {
 	int x = 11;
     cout << x << endl; // wartosc zmiennej
-    cout << &x << endl;  // 0x oznacza wartosc szestnastkowa
-    cout << * &x << endl;
+    cout << &x << endl;  // 0x oznacza wartosc szestnastkowa adres zmiennej pamieci
+    cout << * &x << endl; // wartosc zmiennej pod adresem
+    
+    int *px ; // definicja wskaznika do typu calkowitego
+    px = &x ; // inicjacja wskaznika
+    // wskaznik zawsze zawiera adres pamieci
+    cout << px << endl ;
+    cout << *px << endl ;
+    
+    int y = 100;
+    px = &y ;
+    cout << px << endl;
+    cout <<*px << endl;
+    
+    
 	return 0;
 }
 
