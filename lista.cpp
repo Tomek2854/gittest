3036b497e09e3f3c62af6636c9fd88c4a3d95e16
Tomek2854 <twiniarski8@gmail.com>
2018-02-01 13:30:51 +0100

    Lista

---
+++ lista.cpp
@@ -0,0 +1,60 @@
+/*
+ * lista.cpp
+ * 
+
+ * 
+ * 
+ */
+#include <iostream>
+#include "lista.hpp"
+
+Lista::Lista(){
+    head =NULL;
+    tail =NULL;
+}
+
+Lista::~Lista(){
+    while(Usun()) {;};
+}
+
+void Lista::Dodaj(int wartosc){
+        ELEMENT *el = new ELEMENT;
+        el->wartosc = wartosc;
+        el->nast = NULL;
+        if(head == NULL) {
+            head = el;
+            tail = el;
+        } else {
+            tail ->nast = el;   //ustawienie wskaznika nast dotychczasowego
+            // ostatniego elementu na adres nowego ostatniego elementu
+            tail = el; // aktualinujemy wskaznik ogon, aby wskazywal nowy element
+        }
+}
+void Lista::Wyswietl() {
+    ELEMENT *el = head;
+    while (el != NULL){
+        std::cout <<el->wartosc << " ";
+        el =  el->nast;
+    }
+    std::cout << std::endl;
+}
+
+bool Lista::Usun(){
+    if (head !=NULL) {
+        if(head == tail) {
+            delete head;
+            head = NULL;
+            tail = NULL;
+        } else {
+            ELEMENT *el = head;
+            while(el->nast !=tail){ //szukam przedostatniego elementu
+                el = el->nast; //zapis adresu przedostatniego elementu
+            }
+            delete el->nast;
+            el->nast = NULL;
+            tail = el;
+        }
+        return true;
+    }
+    return false;    
+}
---
+++ lista.hpp
@@ -0,0 +1,23 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+struct ELEMENT{
+    int wartosc;
+    ELEMENT *nast; //wskaznik do nastepnego elementu listy
+};    
+
+class Lista {
+    private:    //hermetyzacja danych
+        ELEMENT *head;
+        ELEMENT *tail;
+    public:     //interfejs publiczny (API klasy)
+        Lista();//konstruktor
+        ~Lista();//destruktor
+        // memory leaks - wycieki pamieci
+        void Dodaj(int);
+        void Wyswietl();
+        bool Usun();
+        void Wstaw();
+};
+
+#endif
